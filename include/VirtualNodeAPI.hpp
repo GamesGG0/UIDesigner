d30fdc9af19f7d57708de66f753f58ded67d9653
camila314 <47485054+camila314@users.noreply.github.com>
2025-11-23 22:11:39 -0600

    ts pmo man

--- include/VirtualNodeAPI.hpp
+++ include/VirtualNodeAPI.hpp
@@ -6,6 +6,12 @@
 #define MY_MOD_ID "camila314.uidesigner"
 using namespace geode::prelude;
 
+#ifndef GEODE_DEFINE_EVENT_EXPORTS
+#define $_export(...) GEODE_EVENT_EXPORT_NORES(__VA_ARGS__)
+#else
+#define $_export(...)
+#endif
+
 namespace uidesigner {
     class VirtualNode : public CCNode {
         friend class VirtualDOMManager;
@@ -18,31 +24,33 @@ namespace uidesigner {
     public:
         VirtualNode() { init(); }
 
-        inline bool init() override;
-        inline void addChild(CCNode* a, int b, int c) override;
+        bool init() override $_export(&VirtualNode::init, (this));
+        void addChild(CCNode* a, int b, int c) override $_export(&VirtualNode::addChild, (this, a, b, c));
+        void removeFromParent() override $_export(&VirtualNode::removeFromParent, (this));
+        void update(float dt) override $_export(&VirtualNode::update, (this, dt));
 
-        inline void removeFromParent() override;
-        inline void update(float dt) override;
         void replaceTether(CCNode*) GEODE_EVENT_EXPORT_NORES(&VirtualNode::replaceTether, (this, std::declval<CCNode*>()));
 
-        inline virtual void settings();
-        inline virtual void updateTether();
-        inline virtual std::string emitCode(int indent = 0);
-        inline virtual void addTetherChild(VirtualNode* a);
-        inline virtual matjson::Value exportJSON();
-        inline virtual void importJSON(matjson::Value value);
+        virtual void settings() $_export(&VirtualNode::settings, (this));
+        virtual void updateTether() $_export(&VirtualNode::updateTether, (this));
+        virtual std::string emitCode(int indent = 0) $_export(&VirtualNode::emitCode, (this, indent));
+        virtual void addTetherChild(VirtualNode* a) $_export(&VirtualNode::addTetherChild, (this, a));
+        virtual matjson::Value exportJSON() $_export(&VirtualNode::exportJSON, (this));
+        virtual void importJSON(matjson::Value value) $_export(&VirtualNode::importJSON, (this, value));
 
         std::string emitAttributes(matjson::Value json, int indent = 0) GEODE_EVENT_EXPORT_NORES(&VirtualNode::emitAttributes, (this, json, indent));
     };
 
-    bool VirtualNode::init() GEODE_EVENT_EXPORT_NORES(&VirtualNode::init, (this));
-    void VirtualNode::addChild(CCNode* a, int b, int c) GEODE_EVENT_EXPORT_NORES(&VirtualNode::addChild, (this, a, b, c));
-    void VirtualNode::removeFromParent() GEODE_EVENT_EXPORT_NORES(&VirtualNode::removeFromParent, (this));
-    void VirtualNode::update(float dt) GEODE_EVENT_EXPORT_NORES(&VirtualNode::update, (this, dt));
-    void VirtualNode::settings() GEODE_EVENT_EXPORT_NORES(&VirtualNode::settings, (this));
-    void VirtualNode::updateTether() GEODE_EVENT_EXPORT_NORES(&VirtualNode::updateTether, (this));
-    std::string VirtualNode::emitCode(int indent) GEODE_EVENT_EXPORT_NORES(&VirtualNode::emitCode, (this, indent));
-    void VirtualNode::addTetherChild(VirtualNode* a) GEODE_EVENT_EXPORT_NORES(&VirtualNode::addTetherChild, (this, a));
-    matjson::Value VirtualNode::exportJSON() GEODE_EVENT_EXPORT_NORES(&VirtualNode::exportJSON, (this));
-    void VirtualNode::importJSON(matjson::Value value) GEODE_EVENT_EXPORT_NORES(&VirtualNode::importJSON, (this, value));
+    #ifdef GEODE_DEFINE_EVENT_EXPORTS
+        GEODE_EVENT_EXPORT_NORES(&VirtualNode::init, (this));
+        GEODE_EVENT_EXPORT_NORES(&VirtualNode::addChild, (this, a, b, c));
+        GEODE_EVENT_EXPORT_NORES(&VirtualNode::removeFromParent, (this));
+        GEODE_EVENT_EXPORT_NORES(&VirtualNode::update, (this, dt));
+        GEODE_EVENT_EXPORT_NORES(&VirtualNode::settings, (this));
+        GEODE_EVENT_EXPORT_NORES(&VirtualNode::updateTether, (this));
+        GEODE_EVENT_EXPORT_NORES(&VirtualNode::emitCode, (this, indent));
+        GEODE_EVENT_EXPORT_NORES(&VirtualNode::addTetherChild, (this, a));
+        GEODE_EVENT_EXPORT_NORES(&VirtualNode::exportJSON, (this));
+        GEODE_EVENT_EXPORT_NORES(&VirtualNode::importJSON, (this, value));
+    #endif
 }
